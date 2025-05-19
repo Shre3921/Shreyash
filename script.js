79179ef3a5517d0d8f1f50a1210d75d3f83457e8
Shre3921 <shreyashsubhash3921@gmail.com>
2025-05-19 11:24:57 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-// Responsive Navbar (optional dropdown for mobile)
-document.addEventListener("DOMContentLoaded", () => {
-  const navLinks = document.querySelector('.nav-links');
-
-  // If you want a burger menu, add it in HTML and add toggle logic here
-});
-
+console.log('Welcome to Shreyash's Digital Identity Website');
