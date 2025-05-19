9f33afca9f40e8f60a7c5abd636c0aac71372a49
Shre3921 <shreyashsubhash3921@gmail.com>
2025-05-19 10:30:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Responsive Navbar (optional dropdown for mobile)
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelector('.nav-links');
+
+  // If you want a burger menu, add it in HTML and add toggle logic here
+});
+
